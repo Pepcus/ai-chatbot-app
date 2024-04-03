46a13d42ba8f462bdebd2bd6357530d3c9fb660d
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-03 16:19:59 +0530

    test commit

--- app/share/[id]/page.tsx
+++ app/share/[id]/page.tsx
@@ -48,10 +48,11 @@ export default async function SharePage({ params }: SharePageProps) {
             </div>
           </div>
         </div>
+        <AI>
+          <ChatList messages={uiState} isShared={true} />
+        </AI>
       </div>
-      <AI>
-        <ChatList messages={uiState} isShared={true} />
-      </AI>
+
       <FooterText className="py-8" />
     </>
   )
