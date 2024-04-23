bdd851ddea9823fc7254b789e92157b7ef786e87
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-23 16:39:20 +0530

    Fixed lint issues.

--- components/responses/response.tsx
+++ components/responses/response.tsx
@@ -2,11 +2,11 @@ interface Response {
   description: string
 }
 
-export function Response({ props: response }: { props: Response }) {
+export function Response({ props: response }: { props: string }) {
   return (
     <div className="-mt-2 flex w-full flex-col gap-2 py-4">
       <div className="text-zinc-500">
-        {response.description}
+        {response}
       </div>
     </div>
   )
