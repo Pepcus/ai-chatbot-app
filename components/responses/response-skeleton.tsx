49d9175be8909133ef88dd96856a097eff11440b
Deepak Nigam <deepak.nigam@pepcus.com>
2024-04-23 18:05:15 +0530

    Corrected the skeleton.

--- components/responses/response-skeleton.tsx
+++ components/responses/response-skeleton.tsx
@@ -2,7 +2,7 @@ export const ResponseSkeleton = () =>  {
   return (
     <div className="-mt-2 flex w-full flex-col gap-2 py-4">
       <div className="text-zinc-500">
-        XXXXXXX
+        
       </div>
     </div>
   )
